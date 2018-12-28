01bf935d7d67b53c1244a09ecbc806b9a4e93bc3
Peter <peter.prentiss@gmail.com>
2018-12-27 18:28:47 -0600

    Import firebase into App

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyAygXROPdc4OVJa4-kIHgUdtrvx5gddekU",
+            authDomain: "manager-c9cc3.firebaseapp.com",
+            databaseURL: "https://manager-c9cc3.firebaseio.com",
+            projectId: "manager-c9cc3",
+            storageBucket: "manager-c9cc3.appspot.com",
+            messagingSenderId: "574423621056"
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
