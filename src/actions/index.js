5fcc6b781d776b66cc2b7988e41f03c48d7e226b
Peter <peter.prentiss@gmail.com>
2019-01-28 06:54:33 -0600

    import firebase and create loginUser

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import { EMAIL_CHANGED,
          PASSWORD_CHANGED
 } from './types';
@@ -14,4 +15,8 @@ export const passwordChanged = (text) => {
         type: PASSWORD_CHANGED,
         payload: text
     };
+};
+
+export const loginUser = ({ email, password}) => {
+
 };
